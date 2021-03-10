96ebf558a0f4e3222b2cad6f69cdbdc9bf7dec27
vadimshut <vadimshut1992@gmail.com>
2021-03-10 11:09:03 +0300

    feat: implement reverse finction

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let new_number = 0;
+    let whole_part = n < 0 ? -n : n;
+  while (whole_part > 0){
+    let remainder = whole_part % 10;
+    new_number = (new_number * 10) + remainder;
+    whole_part = Math.trunc(whole_part/10);
+  }
+  return new_number
 }
